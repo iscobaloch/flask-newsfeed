45f091f1e8bf7a7dc2bee2910b793d3f08eb8fde
Wali Baloch <iscobaloch>
2021-10-10 09:17:24 -0400

    changes

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 
 db = SQLAlchemy()
 app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://admin:12345678@localhost:3306/newsfeed"
+app.config["SQLALCHEMY_DATABASE_URI"]="postgres+psycopg2://sxlbimfcblaxsg:933392f77dee46ead42e658021f71f55548b3094bf9199a1e8d4a4163eb96936@ec2-3-209-65-193.compute-1.amazonaws.com:5432/d5altodeq2cn5f
+"
 app.config['SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 app.config['UPLOAD_FOLDER'] = '/home/wali/PycharmProjects/newsfeed/static/upload/thumbnail/'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
