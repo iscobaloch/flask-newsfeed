b6b7207a06479e5c50a4486deef9a01467ce2edc
Wali Baloch <iscobaloch>
2021-10-13 14:03:15 -0400

    make it better

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = SQLAlchemy()
 app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 app.config["SQLALCHEMY_DATABASE_URI"]= 'sqlite:///' + os.path.join(basedir, 'backup.db')
 app.config['SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
-app.config['UPLOAD_FOLDER'] = '/app/static/upload/thumbnail'
+app.config['UPLOAD_FOLDER'] = os.path.join(basedir,'static/upload/thumbnail')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Initialize 
